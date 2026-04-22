27d0284fe45e38947dc497433a63e4ccae459c15
dongliqing <18758053497@163.com>
2026-04-22 16:31:19 +0800

    fix: 修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
-import uni from '@dcloudio/vite-plugin-uni';
+import uni_plugin from '@dcloudio/vite-plugin-uni';
 import AutoImport from 'unplugin-auto-import/vite';
-import Unocss from 'unocss/vite';
+import UnoCSS from 'unocss/vite';
 
-// https://vitejs.dev/config/
+// @ts-ignore
+const uni = uni_plugin.default || uni_plugin;
+
+// https://vitejs.dev/config/pnpm run dev:mp-weixin
 export default defineConfig({
   plugins: [
     uni(),
-    Unocss(),
+    UnoCSS(),
     AutoImport({
       imports: ['vue', 'uni-app', 'pinia'],
       dts: 'src/auto-import.d.ts',
